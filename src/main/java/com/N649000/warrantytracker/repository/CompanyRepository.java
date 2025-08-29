d7c4363e58db557b2fb0d5ee7bd25d6f70c21a7c
649000 <649000@users.noreply.github.com>
2025-08-29 17:23:25 +0800

    Clean start for Quarkus

--- src/main/java/com/N649000/warrantytracker/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.N649000.warrantytracker.repository;
-
-import com.N649000.warrantytracker.model.Company;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
-
-@EnableScan
-public interface CompanyRepository extends CrudRepository<Company, String> {
-}
