f2db3e56448ff7e890f88b5f479adfab2adc105a
Nazri <649000@users.noreply.github.com>
2025-09-01 20:36:46 +0800

    refactor: remove unused JsonWebToken import and injection

--- api/src/main/java/com/nazri/resource/ClaimResource.java
+++ api/src/main/java/com/nazri/resource/ClaimResource.java
@@ -1,15 +1,15 @@
 package com.nazri.resource;
 
 import com.nazri.model.Claim;
-import com.nazri.service.ClaimService;
-import com.nazri.service.UserService;
 import com.nazri.model.User;
 import com.nazri.model.Warranty;
+import com.nazri.service.ClaimService;
+import com.nazri.service.UserService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.jwt.JsonWebToken;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,9 +24,6 @@ public class ClaimResource extends BaseResource {
 
     @Inject
     UserService userService;
-
-    @Inject
-    JsonWebToken jwt;
 
     @GET
     public Response getUserClaims() {
